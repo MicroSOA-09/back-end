89b779c9e908d3e9da2ca71041ca2e303f3e80f8
Slobodan Obradovic <slobica@192.168.1.35>
2025-04-13 12:32:48 +0200

    Added Test Endopoint for health check

--- src/Explorer.API/Controllers/TestController.cs
+++ src/Explorer.API/Controllers/TestController.cs
@@ -6,46 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Explorer.API.Controllers;
 
-[Route("api/users")]
-public class AuthenticationController : BaseApiController
+[Route("api/test")]
+public class TestController : BaseApiController
 {
-    private readonly IAuthenticationService _authenticationService;
-
-    public AuthenticationController(IAuthenticationService authenticationService)
+    [HttpGet]
+    public IActionResult Get()
     {
-        _authenticationService = authenticationService;
-    }
-
-    [HttpPost]
-    public ActionResult<AuthenticationTokensDto> RegisterTourist([FromBody] AccountRegistrationDto account)
-    {
-        var result = _authenticationService.RegisterTourist(account);
-        return CreateResponse(result);
-    }
-
-    [HttpPost("login")]
-    public ActionResult<AuthenticationTokensDto> Login([FromBody] CredentialsDto credentials)
-    {
-        var result = _authenticationService.Login(credentials);
-        return CreateResponse(result);
-    }
-
-    public class PasswordResetRequestDto
-    {
-        public string Email { get; set; }
-    }
-
-    [HttpPost("request")]
-    public ActionResult<string> RequestPasswordReset([FromBody] PasswordResetRequestDto request)
-    {
-        var result = _authenticationService.RequestPasswordReset(request.Email);
-        return CreateResponse(result);
-    }
-
-    [HttpPost("reset")]
-    public ActionResult<string> ResetPassword([FromBody] PasswordResetDto request)
-    {
-        var result = _authenticationService.ResetPassword(request);
-        return CreateResponse(result);
+        return Ok(new { message = "API is working" });
     }
 }
